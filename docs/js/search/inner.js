98943968933662e42f8be302933e79a95a891575
NazarovIlya <eliakohai@gmail.com>
2023-03-12 08:35:48 +0300

    updated

--- docs/js/search/inner.js
+++ docs/js/search/inner.js
@@ -1,13 +1,28 @@
 
 const text = document.querySelector('.searchResults');
-// text.innerHTML = localStorage.getItem('discription');
-
 let json = localStorage.getItem("json");
 let obj = JSON.parse(json);
 text.innerHTML = obj;
 let str = "";
 for (let i = 0; i < obj.length; i++) {
-    str += `<p>${obj[i].link}<br>${obj[i].description}</p>`;
-        "<p>" + obj[i].link + obj[i].description + "</p>";
+    str += `<p>${obj[i].link}<br>${obj[i].text}</p>`;
 }
 text.innerHTML = str;
+
+// const url_1 = 'index.html';
+// fetch(url_1)
+//     .then((response) => {
+//         console.log(response);
+//         console.log(response.url);
+//     });
+
+// fetch("url")
+//   .then(res => res.text())
+//   .then(html => console.log(html))
+
+// fetch(url_1)
+//     .then(res => res.text())
+//     .then(html => console.log(new DOMParser()
+//         .parseFromString(html, "text/xml")
+//         .querySelector("body")));
+

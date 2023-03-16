b518f19aa61c1de8a7ec7d837319a84a0679ff9b
NazarovIlya <eliakohai@gmail.com>
2023-03-17 01:02:37 +0300

    mistake corrected, search algorithm updared

--- docs/js/search/inner.js
+++ docs/js/search/inner.js
@@ -4,6 +4,6 @@ let json = localStorage.getItem("json");
 let obj = JSON.parse(json);
 let str = "";
 for (let i = 0; i < obj.length; i++) {
-    str += `<p>${obj[i].link}<br>${obj[i].description}</p>`;
+    str += `<p><a href="${obj[i].link}">${obj[i].title}</a></p><p>${obj[i].description}</p>`;
 }
 text.innerHTML = str;

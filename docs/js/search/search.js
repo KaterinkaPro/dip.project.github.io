92612f82c776354238cf5ebe6dcd51ca165a2bac
NazarovIlya <eliakohai@gmail.com>
2023-03-19 02:30:37 +0300

    five articles added into search db

--- docs/js/search/search.js
+++ docs/js/search/search.js
@@ -13,6 +13,11 @@ async function main() {
     let onega_lake = null;
     let petroglyphs = null;
     let ruskeala = null;
+    let epos = null;
+    let legends = null;
+    let sigvard = null;
+    let giants = null;
+    let monster = null;
 
     const response = await fetch('index.html');
     if (response.ok) {
@@ -26,6 +31,11 @@ async function main() {
         onega_lake = await getText('attractions/onega_lake.html');
         petroglyphs = await getText('attractions/petroglyphs.html');
         ruskeala = await getText('attractions/ruskeala.html');
+        epos = await getText('epos.html');
+        legends = await getText('legends.html');
+        sigvard = await getText('legends/captain_sivgard.html');
+        giants = await getText('legends/giants.html');
+        monster = await getText('legends/monster.html')
     } else {
         index = await getText('../index.html');
         history = await getText('../about_karelia/history.html');
@@ -37,23 +47,29 @@ async function main() {
         onega_lake = await getText('../attractions/onega_lake.html');
         petroglyphs = await getText('../attractions/petroglyphs.html');
         ruskeala = await getText('../attractions/ruskeala.html');
+        epos = await getText('epos.html');
+        legends = await getText('legends.html');
+        sigvard = await getText('../legends/captain_sivgard.html');
+        giants = await getText('../legends/giants.html');
+        monster = await getText('../legends/monster.html')
     }
-
+    //! TODO keywords
     const indexObj = {title: "О Карелии - главная страница", link: "index.html", keywords: "qwerty", description: "Очень долго республика Карелия была широко известна в&nbsp;узких кругах любителей дикого туризма, охоты и&nbsp;рыбалки, но&nbsp;теперь все изменилось. Завораживающие скалы, обилие невероятных водопадов, будоражащие воображение болота, и&nbsp;множество троп, уходящих вглубь Тайги&nbsp;&mdash; это всё среди нас, это все&nbsp;&mdash; Карелия. Карелия открылась для всех видов туризма. Нет такого направления туризма, который нельзя встретить в&nbsp;этом дивном крае", text: index };    
     const historyObj = { title: "История Карелии", link: "about_karelia/history.html", keywords: "qwerty", description: "В&nbsp;процессе миграции людей по&nbsp;территориям, прилегающим к&nbsp;Карелии, исторически сложились три группы племён: вепсы, жившие между Ладожским и&nbsp;Онежским озерами; саами (лопари), занимавшие практически всю территорию современной Карелии и&nbsp;карелы, жившие к&nbsp;западу и&nbsp;северу от&nbsp;Ладожского озера. Само название Карелия приобрела благодаря одноименному населению. Основными занятиями древнейших жителей являлись охота и&nbsp;рыболовство. Об&nbsp;их&nbsp;жизни и&nbsp;быте нам помогают узнать археологические находки и&nbsp;сохранившиеся до&nbsp;наших времен наскальные рисунки&nbsp;&mdash; петроглифы.", text: history };
     const natureObj = { title: "Природа Карелии", link: "about_karelia/nature.html", keywords: "qwerty", description: "Природа Карелии очаровывает любого, кто хоть единожды бывал в&nbsp;этих местах. И&nbsp;это неслучайно. Красивая северная природа изобилует буйными реками с&nbsp;крутыми порогами, девственно чистыми лесами, разнообразной флорой и&nbsp;фауной. В&nbsp;Карелии насчитывается более 60&nbsp;000&nbsp;озер, самые известные и&nbsp;самые крупные из&nbsp;которых&nbsp;&mdash; Онежское и&nbsp;Ладожское. В&nbsp;республике протекает множество рек и&nbsp;речушек, но&nbsp;все они короткие. Самая длинная из&nbsp;них&nbsp;&mdash; Кемь протяженностью в&nbsp;360&nbsp;км. Также есть и&nbsp;болота и&nbsp;водопады. Именно водоемы в&nbsp;сочетании с&nbsp;карельскими лесами и&nbsp;создают тот удивительный климат, который очаровывает всех.", text: nature };    
     const atrractionsObj = { title: "Достопримечательности Карелии", link: "attractions.html", keywords: "qwerty", description: "Ладожское озеро&nbsp;&mdash; настоящая жемчужина Карелии и Ленинградской области. Его суровая и&nbsp;величественная красота волнует людей не одно столетие. Такие великие художники, как Шишкин, Клодт, Куинджи, посещали эти места и&nbsp;передавали их&nbsp;красоту в&nbsp;своих полотнах. Но&nbsp;можно&nbsp;ли передать те&nbsp;эмоции и впечатления от&nbsp;пребывания у&nbsp;Ладоги!", text: attractions };
-    
-    //! TODO
     const valaam_islandObj = { title: "Остров Валаам", link: "attractions/valaam_island.html",  description: "Валаамский архипелаг на&nbsp;Ладожском озере&nbsp;&mdash; это один из&nbsp;старейших центров русского православия. Монастырь здесь был основан не&nbsp;позднее начала XV&nbsp;века, а&nbsp;по&nbsp;некоторым данным еще раньше&nbsp;&mdash; в&nbsp;XI-XII&nbsp;веках. У&nbsp;монастырского комплекса сложная история&nbsp;&mdash; его территория входила в&nbsp;состав разных стран и&nbsp;становилась местом военных действий, в&nbsp;некоторые периоды монахи покидали его. Однако в&nbsp;1990-х началось возрождение этого православного центра.", keywords: "qwerty", text: valaam_island };
     const kizhi_islandObj = { title: "Остров Кижи", link: "attractions/kizhi_island.html", description: "Остров Кижи, расположенный на&nbsp;севере Онежского озера,&nbsp;&mdash; это настоящий музей под открытым небом. Здесь находится несколько десятков памятников деревянной северорусской архитектуры. Среди них есть как здания, построенные на&nbsp;самом острове, так и&nbsp;привезенные сюда из&nbsp;разных районов Карелии.", keywords: "qwerty", text: kizhi_island };
     const ladoga_lakeObj = { title: "Ладожское озеро", link: "attractions/lake_ladoga.html", description: "Ладога&nbsp;&mdash; крупнейшее пресноводное озеро Европы, его северная часть находится на&nbsp;территории Карелии. Живописные берега и&nbsp;сотни островов привлекают туристов не&nbsp;только своей природой, но&nbsp;и&nbsp;уникальными культурными объектами.", keywords: "qwerty", text: ladoga_lake };
     const onega_lakeObj = { title: "Онежское озеро", link: "attractions/onega_lake.html", description: "Онежское озеро&nbsp;&mdash; объект важного хозяйственного значения. Оно расположено на&nbsp;северо-западе России в&nbsp;нескольких регионах. Большая часть водоема, а&nbsp;это&nbsp;80%, заходит в&nbsp;республику Карелию. Оставшиеся&nbsp;20% водной поверхности делят Ленинградская и&nbsp;Вологодская области. Огромный пресноводный водоем пользуется популярностью у&nbsp;туристов. Путешественников привлекают природные красоты северного края, древняя история.", keywords: "qwerty", text: onega_lake };
     const petroglyphsObj = { title: "Петроглифы", link: "attractions/petroglyphs.html", description: "Петроглифы Карелии&nbsp;&mdash; древние наскальные рисунки, выбитые на&nbsp;плоских камнях на&nbsp;восточном побережье озера Онега. Каменные изображения представляют огромную научную и&nbsp;художественную ценность и&nbsp;являются памятниками монументального искусства первобытной эпохи. В&nbsp;них можно заметить влияние материального и&nbsp;нематериального мира на&nbsp;жизнь древнего человека.", keywords: "qwerty", text: petroglyphs };
     const ruskealaObj = { title: "Горный парк &laquo;Рускеала&raquo;", link: "attractions/ruskeala.html", description: "В&nbsp;XVIII веке на&nbsp;территории Сортавальского района Карелии добывали мрамор&nbsp;&mdash; именно отсюда привозился камень для украшения Эрмитажа и&nbsp;многих других зданий Петербурга. Месторождение разрабатывалось до&nbsp;середины XIX&nbsp;века, а&nbsp;затем было почти заброшено. Постепенно ландшафт возвращался в&nbsp;природное состояние: мраморный карьер заполнился водой и&nbsp;превратился в&nbsp;живописный водоем. Сейчас здесь находится горный парк &laquo;Рускеала&raquo;", keywords: "qwerty", text: ruskeala };
-    // const nameObj = { title: "", link: "", description: "", keywords: "qwerty", text: name7 };
-    // const nameObj = { title: "", link: "", description: "", keywords: "qwerty", text: name8 };
-
+    const eposObj = { title: "Эпос", link: "epos.html", description: "", keywords: "qwerty", text: epos }; //! TODO description, keywords
+    const legendsObj = { title: "Легенды Карелии", link: "legends.html", description: "", keywords: "qwerty", text: legends }; //! TODO description, keywords
+    const sigvardObj = { title: "Капитан Сигвард", link: "legends/captain_sivgard.html", description: "Север хранит в&nbsp;себе множество тайн и&nbsp;загадок. Многие легенды и&nbsp;мифы передаются из&nbsp;уст в&nbsp;уста, из&nbsp;поколения в&nbsp;поколение, и&nbsp;у&nbsp;каждого народа свой фольклор.", keywords: "qwerty", text: sigvard }; //! TODO description, keywords
+    const giantsObj = { title: "О Великинах", link: "legends/giants.html", description: "На&nbsp;Ладожском побережье существует предание, что когда-то в&nbsp;этих местах жили огромные люди, так называемые метелиляйнены, или мунккилайнены, которых постепенно вытеснили отсюда лапландцы и&nbsp;финны. Само название пошло от&nbsp;финского &laquo;meteli&raquo;&nbsp;&mdash; шум, все потому, что во&nbsp;время их&nbsp;передвижения по&nbsp;лесам, они создавали много шума. До&nbsp;сих пор люди обнаруживают следы их&nbsp;существования: огромные кости и&nbsp;плуги. Также есть странные места с&nbsp;нагромождением огромных валунов.", keywords: "qwerty", text: giants }; //! TODO description, keywords
+     const monsterObj = { title: "Неизвестный монстр или тайна Ладожского озера", link: "legends/monster.html", description: "Я&nbsp;не&nbsp;знаю ни&nbsp;одного глубоководного озера, в&nbsp;котором хотя&nbsp;бы раз не&nbsp;было замечено некое явление, не&nbsp;поддающееся логическому и&nbsp;научному объяснению.", keywords: "qwerty", text: monster }; //! TODO description, keywords
+    // const nameObj = { title: "", link: "", description: "", keywords: "qwerty", text: name };
 
     // Заполнение массива JSON-объектами: заглавие, ссылка, описание, ключевые слова, текст
     //! TODO
@@ -68,17 +84,13 @@ async function main() {
     textsArray.push(onega_lakeObj);
     textsArray.push(petroglyphsObj);
     textsArray.push(ruskealaObj);
-    // textsArray.push();
-    // textsArray.push();
-    // textsArray.push(nameObj1);
-    // textsArray.push(nameObj2);
-    // textsArray.push(nameObj3);
-    // textsArray.push(nameObj4);
-    // textsArray.push(nameObj5);
-    // textsArray.push(nameObj6);
-    // textsArray.push(nameObj7);
-    // textsArray.push(nameObj8);
-    
+    textsArray.push(eposObj);
+    textsArray.push(legendsObj);
+    textsArray.push(sigvardObj);
+    textsArray.push(giantsObj);
+    textsArray.push(monsterObj);
+    // textsArray.push(nameObj);
+      
     async function getText(url) {
         const resp = await fetch(url);
         const text = await resp.text();
